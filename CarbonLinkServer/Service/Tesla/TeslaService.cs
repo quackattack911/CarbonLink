72234b90087a6cbbde9b9d17bff6cb614caeaa06
quackattack911 <31573958+quackattack911@users.noreply.github.com>
2021-11-23 17:19:00 -0800

    complete tesla service

--- CarbonLinkServer/Service/Tesla/TeslaService.cs
+++ CarbonLinkServer/Service/Tesla/TeslaService.cs
@@ -1,3 +1,6 @@
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
 namespace CarbonLinkServer.Service.Tesla;
 
 public class TeslaService
@@ -5,16 +8,40 @@ public class TeslaService
     private readonly IConfiguration _configuration;
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly string _accessToken;
+    private readonly string _id;
 
     public TeslaService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
     {
         _configuration = configuration;
         _httpClientFactory = httpClientFactory;
         _accessToken = _configuration["Tesla:AccessToken"];
+        _id = _configuration["Tesla:Id"];
     }
 
     public async Task<ChargeStateDto> GetChargeState()
     {
-        var httpClient = _httpClientFactory.CreateClient("Tesla");
+        try
+        {
+            string chargeRoute = $"api/1/vehicles/{_id}/data_request/charge_state";
+            var httpClient = _httpClientFactory.CreateClient("Tesla");
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+            var httpResponseMessage = await httpClient.PostAsync(chargeRoute, null);
+            if (httpResponseMessage.IsSuccessStatusCode && httpResponseMessage.Content != null)
+            {
+                string responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
+                var settings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    MissingMemberHandling = MissingMemberHandling.Ignore
+                };
+                return JsonConvert.DeserializeObject<ChargeStateDto>(responseContent, settings);
+            }
+            throw new Exception("Get Charge State Failed!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        
     }
 }
