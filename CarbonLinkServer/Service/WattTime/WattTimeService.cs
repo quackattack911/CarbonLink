3eb7d465abbd9d4a922a6e096e76ebad4e120627
quackattack911 <31573958+quackattack911@users.noreply.github.com>
2021-11-24 01:40:27 -0800

    watt time login and real time index

--- CarbonLinkServer/Service/WattTime/WattTimeService.cs
+++ CarbonLinkServer/Service/WattTime/WattTimeService.cs
@@ -1,1 +1,64 @@
+
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
 namespace CarbonLinkServer.Service.WattTime;
+
+public class WattTimeService
+{
+    private readonly IConfiguration _configuration;
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly string _username;
+    private readonly string _password;
+
+    public WattTimeService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+    {
+        _configuration = configuration;
+        _httpClientFactory = httpClientFactory;
+        _username = _configuration["WattTime:Username"];
+        _password = _configuration["WattTime:Password"];
+    }
+
+    public async Task<RealTimeEmissionsIndexDto> GetRealTimeEmissions()
+    {
+        string realTimeEmissionRoute = "index";
+        var httpClient = _httpClientFactory.CreateClient("WattTime");
+        var accessToken = await Login();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Token);
+        var httpResponseMessage = await httpClient.GetAsync(realTimeEmissionRoute);
+
+        if (!httpResponseMessage.IsSuccessStatusCode && httpResponseMessage.Content == null)
+        {
+            throw new Exception("Get Real Time Emissions Failed!");
+        }
+
+        string responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
+        var settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore
+        };
+        RealTimeEmissionsIndexDto realTimeEmissionsIndexDto = JsonConvert.DeserializeObject<RealTimeEmissionsIndexDto>(responseContent, settings);
+        return realTimeEmissionsIndexDto;
+    }
+
+    private async Task<AccessTokenDto> Login()
+    {
+        string loginRoute = "login";
+        var httpClient = _httpClientFactory.CreateClient("WattTime");
+        //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+        var httpResponseMessage = await httpClient.GetAsync(loginRoute);
+        if (!httpResponseMessage.IsSuccessStatusCode && httpResponseMessage.Content == null)
+        {
+            throw new Exception("Watt Time Login Failed!");
+        }
+        string responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
+        var settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore
+        };
+        AccessTokenDto accessToken = JsonConvert.DeserializeObject<AccessTokenDto>(responseContent, settings);
+        return accessToken;
+    }
+}
