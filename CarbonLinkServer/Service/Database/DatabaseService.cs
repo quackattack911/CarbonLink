62265ba831238904f5d7de52f1adf66f81e4d4b9
quackattack911 <31573958+quackattack911@users.noreply.github.com>
2021-11-27 11:19:48 -0800

    create database crud service

--- CarbonLinkServer/Service/Database/DatabaseService.cs
+++ CarbonLinkServer/Service/Database/DatabaseService.cs
@@ -11,7 +11,30 @@ namespace CarbonLinkServer.Service.Database
         public DatabaseService(IConfiguration configuration)
         {
             _configuration = configuration;
-
+            MongoClient mongoClient = new MongoClient(_configuration["MongoDb:ConnectionString"]);
+            var database = mongoClient.GetDatabase(_configuration["MongoDb:DatabaseName"]);
+            _users = database.GetCollection<User>(_configuration["MongoDb:CollectionName"]);
         }
+
+        public List<User> Get() =>
+            _users.Find(user => true).ToList();
+
+        public User Get(string walletAddress) =>
+            _users.Find(user => user.WalletAddress == walletAddress).FirstOrDefault();
+
+        public User Create(User user)
+        {
+            _users.InsertOne(user);
+            return user;
+        }
+
+        public void Update(string walletAddress, User userIn) =>
+            _users.ReplaceOne(user => user.WalletAddress == walletAddress, userIn);
+
+        public void Remove(User userIn) =>
+            _users.DeleteOne(user => user.WalletAddress == userIn.WalletAddress);
+
+        public void Remove(string walletAddress) =>
+            _users.DeleteOne(user => user.WalletAddress == walletAddress);
     }
 }
