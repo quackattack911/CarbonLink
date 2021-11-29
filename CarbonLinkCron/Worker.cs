23e9866bdea49c0ca32f3bbf09e216a9a88908c7
quackattack911 <31573958+quackattack911@users.noreply.github.com>
2021-11-28 16:33:32 -0800

    finish worker calculations

--- CarbonLinkCron/Worker.cs
+++ CarbonLinkCron/Worker.cs
@@ -8,12 +8,18 @@ namespace CarbonLinkCron
     {
         private readonly ILogger<Worker> _logger;
         private readonly DatabaseService _databaseService;
+        private readonly TeslaService _teslaService;
+        private readonly WattTimeService _wattTimeService;
 
         public Worker(ILogger<Worker> logger,
-            DatabaseService databaseService)
+            DatabaseService databaseService,
+            TeslaService teslaService,
+            WattTimeService wattTimeService)
         {
             _logger = logger;
             _databaseService = databaseService;
+            _teslaService = teslaService;
+            _wattTimeService = wattTimeService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -24,9 +30,17 @@ namespace CarbonLinkCron
                 foreach (var user in users)
                 {
                     _logger.LogInformation("This user is charging: {user}", user.Id);
+                    var chargeState = await _teslaService.GetChargeState(user.TeslaToken, user.TeslaId);
+                    var wattTimeIndex = await _wattTimeService.GetRealTimeEmissions(user.Latitude, user.Longitude);
+                    _logger.LogInformation("the index is {index}", wattTimeIndex.Percent);
+                    var chargeDiff = chargeState.ChargeEnergyAdded - user.ChargeAdded;
+                    var tokensGained = chargeDiff * (100 - wattTimeIndex.Percent);
+                    _logger.LogInformation("This user gained {tokens} more tokens", tokensGained);
+                    _databaseService.UpdateBalance(user.Id, (double)tokensGained);
+                    _databaseService.UpdateChargeState(user.Id, (double)chargeState.ChargeEnergyAdded);
                 }
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(60000, stoppingToken);
             }
         }
     }
